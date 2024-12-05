4297cc32080006f2fdbecf4b184d0416b3eeb72d
jeongjihun <wlgns2234@naver.com>
2024-12-06 04:46:03 +0900

    chore: 테이블 설계

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,9 +7,9 @@ CREATE TABLE coupon
     `description`         VARCHAR(512) NOT NULL COMMENT '쿠폰 설명',
     `total_coupon_count`  INT          NOT NULL COMMENT '최초 쿠폰 등록 개수',
     `issued_coupon_count` INT          NOT NULL COMMENT '발급한 쿠폰 개수', -- 수량 감소 예정
-    `CREATED_AT`          TIMESTAMP    NOT NULL COMMENT '테이블 생성 시간',
-    `UPDATED_AT`          TIMESTAMP    NOT NULL COMMENT '테이블 수정 시간',
-    `DELETED_AT`          TIMESTAMP NULL COMMENT '테이블 삭제 시간'
+    `created_at`          TIMESTAMP    NOT NULL COMMENT '쿠폰 생성 시간',
+    `updated_at`          TIMESTAMP    NOT NULL COMMENT '쿠폰 수정 시간',
+    `suspended_at`        TIMESTAMP NULL COMMENT '쿠폰 정지 시간'
 );
 
 DROP TABLE IF EXISTS coupon_issue;
@@ -19,7 +19,7 @@ CREATE TABLE coupon_issue
     `coupon_code` VARCHAR(64) NOT NULL PRIMARY KEY COMMENT '발급 쿠폰 코드',
     `member_id`   BIGINT      NOT NULL COMMENT '발급받은 회원 id',
     `coupon_id`   BIGINT      NOT NULL COMMENT '쿠폰 id',
-    `isUsed`      BOOLEAN     NOT NULL COMMENT '쿠폰 사용 여부',  -- 사용되면 true
-    `CREATED_AT`  TIMESTAMP   NOT NULL COMMENT '테이블 생성 시간', -- 쿠폰 발급 시간
-    `UPDATED_AT`  TIMESTAMP   NOT NULL COMMENT '테이블 수정 시간'  -- 쿠폰 사용 시간
+    `is_used`     BOOLEAN     NOT NULL COMMENT '쿠폰 사용 여부', -- 사용되면 true
+    `created_at`  TIMESTAMP   NOT NULL COMMENT '쿠폰 발급 시간', -- 쿠폰 발급 시간
+    `updated_at`  TIMESTAMP   NOT NULL COMMENT '쿠폰 사용 시간'  -- 쿠폰 사용 시간
 )
